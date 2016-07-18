8e622df44ccd6da3fdbb061c59c49878dfd24393
Felipe Kuninory Akitaya <felipeakitaya@gmail.com>
2016-07-18 01:21:34 -0300

    Commit do projeto de teste da API
    É o mesmo projeto do professor, porém, adaptado para conversar com a API
    do nosso grupo

---
+++ LoginExample/app/src/main/java/br/ufg/inf/es/dsdm/loginexample/services/WebTaskLogin.java
@@ -0,0 +1,70 @@
+package br.ufg.inf.es.dsdm.loginexample.services;
+
+import android.content.Context;
+
+import org.greenrobot.eventbus.EventBus;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import br.ufg.inf.es.dsdm.loginexample.R;
+import br.ufg.inf.es.dsdm.loginexample.model.User;
+
+public class WebTaskLogin extends WebTaskBase {
+
+    private static String SERVICE_URL = "login";
+    private String username;
+    private String senha;
+
+    public WebTaskLogin(Context context, String username, String senha) {
+        super(context, SERVICE_URL);
+        this.username = username;
+        this.senha = senha;
+    }
+
+    @Override
+    public void handleResponse(String response) {
+        User user = new User();
+        try {
+            JSONObject responseAsJSON = new JSONObject(response);
+            String idUser = responseAsJSON.getString("idUser");
+            user.setIdUser(idUser);
+            String username = responseAsJSON.getString("username");
+            user.setUsername(username);
+            String nomeCompleto = responseAsJSON.getString("nomeCompleto");
+            user.setNomeCompleto(nomeCompleto);
+            String email = responseAsJSON.getString("email");
+            user.setEmail(email);
+            EventBus.getDefault().post(user);
+
+        } catch (JSONException e) {
+            if (!isSilent()) {
+                EventBus.getDefault().post(new Error(getContext().getString(R.string.label_error_invalid_response)));
+            }
+        }
+    }
+
+    private User readUser(JSONObject userAsJSON) throws JSONException {
+        User user = new User();
+        user.setIdUser(userAsJSON.getString("idUser"));
+        user.setUsername(userAsJSON.getString("username"));
+        user.setNomeCompleto(userAsJSON.getString("nomeCompleto"));
+        user.setEmail(userAsJSON.getString("email"));
+        return user;
+    }
+
+    @Override
+    public String getRequestBody() {
+        Map<String, String> requestMap = new HashMap<>();
+        requestMap.put("username", username);
+        requestMap.put("senha", senha);
+
+        JSONObject json = new JSONObject(requestMap);
+        String jsonString = json.toString();
+
+        return jsonString;
+    }
+
+}
